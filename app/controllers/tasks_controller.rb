a79404ee23bd4ba80c9c6d57f32a3314b634f63a
Preston Fegley <fegley.preston@gmail.com>
2025-03-05 21:36:04 +0000

    final commit - authorizations for contacts & tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,23 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({ "user_id" => session["user_id"] })
   end
 
   def create
-    @task = Task.new
-    @task["description"] = params["description"]
-    @task.save
+    if session["user_id"].present?
+      @task = Task.new
+      @task["description"] = params["description"]
+      @task["user_id"] = session["user_id"]
+      @task.save
+    end
     redirect_to "/tasks"
   end
 
   def destroy
-    @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    if session["user_id"].present?
+      @task = Task.find_by({ "id" => params["id"], "user_id" => session["user_id"] })
+      @task.destroy
+    end
     redirect_to "/tasks"
   end
 end
